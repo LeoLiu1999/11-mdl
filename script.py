9ed195a68bb7185775bdc4cbed08d6a852275471
TakingTheL <yliu6@stuy.edu>
2018-05-16 15:11:16 -0400

    done!

--- script.py
+++ script.py
@@ -43,6 +43,100 @@ def run(filename):
 
     if p:
         (commands, symbols) = p
+        print p[0]
     else:
         print "Parsing failed."
         return
+
+    for line in commands:
+        print line
+        command = line[0]
+        args = line[1:]
+
+        if command == 'push':
+            stack.append( [x[:] for x in stack[-1]]) #deep copy top-most matrix
+
+        elif command == 'pop':
+            stack.pop()
+
+        elif command == 'move':
+            t = make_translate(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+
+        elif command == 'rotate':
+            theta = float(args[1]) * (math.pi / 180)
+            if args[0] == 'x':
+                t = make_rotX(theta)
+            elif args[0] == 'y':
+                t = make_rotY(theta)
+            else:
+                t = make_rotZ(theta)
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        
+        elif command == 'scale':
+            t = make_scale(float(args[0]), float(args[1]), float(args[2]))
+            matrix_mult( stack[-1], t )
+            stack[-1] = [ x[:] for x in t]
+        
+        elif command == 'line':
+            add_edge( tmp,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), float(args[5]) )
+            matrix_mult( stack[-1], tmp )
+            draw_lines(tmp , screen, zbuffer, color)
+            tmp = []
+
+
+        
+        elif command == 'hermite' or command == 'bezier':
+            add_curve(tmp,
+                      float(args[0]), float(args[1]),
+                      float(args[2]), float(args[3]),
+                      float(args[4]), float(args[5]),
+                      float(args[6]), float(args[7]),
+                      step, line)
+            matrix_mult( stack[-1], tmp )
+            draw_lines(tmp, screen, zbuffer, color)
+            tmp = []
+
+        elif command == 'circle':
+            add_circle(tmp,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step)
+            matrix_mult( stack[-1], tmp )
+            draw_lines(tmp, screen, zbuffer, color)
+            tmp = []
+
+        elif command == 'box':
+            add_box(tmp,
+                    float(args[0]), float(args[1]), float(args[2]),
+                    float(args[3]), float(args[4]), float(args[5]))
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            tmp = []
+
+        elif command == 'torus':
+            add_torus(tmp,
+                      float(args[0]), float(args[1]), float(args[2]),
+                      float(args[3]), float(args[4]), step_3d)
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            tmp = []
+
+        elif command == 'sphere':
+            add_sphere(tmp,
+                       float(args[0]), float(args[1]), float(args[2]),
+                       float(args[3]), step_3d)
+            matrix_mult( stack[-1], tmp )
+            draw_polygons(tmp, screen, zbuffer, view, ambient, light, areflect, dreflect, sreflect)
+            tmp = []
+        
+        elif command == 'display':
+            display(screen)
+
+        elif command == 'save':
+            save_extension(screen, args[0])
+
+    return
